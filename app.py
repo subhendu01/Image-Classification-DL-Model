b249538dbd7dce9c54d003ef641a98a4a38983bf
Subhendu Sekhar Baug <subhendu.baug@gmail.com>
2020-09-11 15:48:05 +0530

    static

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
 # Define a flask app
-app = Flask(__name__, template_folder='templates', static_folder=' static')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
